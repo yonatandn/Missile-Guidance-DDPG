19795064abbd4f15de9543969ada06b6e31b1e5d
yogevat <79969852+yogevat@users.noreply.github.com>
2023-09-11 19:22:59 +0300

    add more things to the code

--- test.py
+++ test.py
@@ -29,6 +29,7 @@ for i_episode in range(20):
         observation, reward, terminated, truncated, info = env.step(action)
 
         if terminated or truncated:
+
             print("Episode finished after {} timesteps".format(t+1))
             break
 
